c553f4df0ddf07c9661a207769d2fe7738947179
Jesus Garnica <gtechy100@gmail.com>
2020-08-23 22:32:49 -0700

    Added Chakra, working on navbar.

---
+++ client/src/components/general/buttons/hamburger.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import styled from 'styled-components';
+export interface NavbarButtonProps {
+  width?: number;
+  active?: boolean;
+  tabIndex?: number;
+  height?: number;
+  onClick?: (mouseEvent: React.MouseEvent) => void;
+  iconColor?: string;
+  ref: React.MutableRefObject<null>;
+}
+/**
+ * @name HamburgerButton
+ * @description Creates a simple button meant to be used
+ * in a nav.
+ * @prop {number} width Sets the total width for the button.
+ * @prop {boolean} active Sets if the icon should be in the "clicked" state.
+ * @prop {number} height Sets the total height for the button
+ * @prop {void} onClick This is fired after the user clicks on the icon. EVENT is passed in.
+ * @prop {string} iconColor Sets the color for the icon. The default is black.
+ *
+ */
+export const HamburgerButton: React.FC<NavbarButtonProps> = ({
+  width = 32,
+  active = false,
+  height = 32,
+  onClick,
+  tabIndex = 1,
+  iconColor,
+  ref = null,
+}) => {
+  let barClassName = '';
+  if (active) {
+    barClassName += 'clicked';
+  }
+  return (
+    <>
+      <Container
+        ref={ref}
+        tabIndex={tabIndex}
+        role="button"
+        onClick={
+          onClick
+            ? onClick
+            : function () {
+                return;
+              }
+        } // If there is an onClick func passed in, do it!
+        width={width}
+        height={height}
+        iconColor={iconColor}
+      >
+        <BarElement iconColor={iconColor} className={'top ' + barClassName} />
+        <BarElement iconColor={iconColor} className={'center ' + barClassName} />
+        <BarElement iconColor={iconColor} className={'bottom ' + barClassName} />
+      </Container>
+    </>
+  );
+};
+
+interface ContainerProps {
+  width: number;
+  height: number;
+  iconColor?: string;
+}
+interface BarElementProps {
+  translateY?: number;
+  iconColor?: string;
+  className?: string;
+}
+const Container = styled.div<React.HtmlHTMLAttributes<HTMLElement> & ContainerProps>`
+  width: ${props => props.width + `px`};
+  height: ${props => props.height + `px`};
+  padding: 4px;
+  cursor: pointer;
+  display: flex;
+  transform: translate3d(0, 0, 0);
+  transform-style: preserve-3d;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  outline: 1px solid transparent;
+  &:focus {
+    outline: 1px solid transparent;
+  }
+  &:active {
+    outline: 1px solid transparent;
+  }
+  -webkit-tap-highlight-color: transparent;
+`;
+const BarElement = styled.div<BarElementProps>`
+  width: 20px;
+  user-select: none;
+  height: 2px;
+  transform: translate3d(0, 0, 0);
+  transform-style: preserve-3d;
+  background: ${props => (props.iconColor ? props.iconColor : `black`)};
+  transition: all 0.2s cubic-bezier(0.215, 0.61, 0.355, 1);
+  &.center {
+    &.clicked {
+      transform: translateX(0px);
+      opacity: 0;
+    }
+    transform: translateX(0);
+    opacity: 1;
+  }
+  &.bottom {
+    margin-top: 5px;
+    transform: none;
+    &.clicked {
+      transform-origin: top left;
+      transform: translateX(-1px) rotate(-45deg);
+    }
+  }
+  &.top {
+    transform: none;
+    margin-bottom: 5px;
+    &.clicked {
+      transform-origin: top left;
+      transform: rotate(45deg);
+    }
+  }
+`;
---
+++ client/src/components/header.tsx
@@ -0,0 +1,95 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import { HamburgerButton } from './general/buttons/hamburger';
+import styled from 'styled-components';
+import {
+  Button,
+  useTheme,
+  Flex,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/core';
+export function Header({ siteTitle }: { siteTitle?: string }): React.ReactElement {
+  const { colors } = useTheme();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef(null);
+  const MenuItems = (
+    <>
+      <Button className="loginButton" variant="outline">
+        Log In
+      </Button>
+      <Button>Sign Up</Button>
+    </>
+  );
+  return (
+    <>
+      <HeaderContainer borderColor={colors.gray[700]}>
+        <NavigationContainer>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: `black`,
+                textDecoration: `none`,
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h1>
+          <Flex display={{ base: 'none', sm: 'flex' }} flexDirection="row" alignItems="center" justifyContent="center">
+            <ButtonsContainer>{MenuItems}</ButtonsContainer>
+          </Flex>
+          <Flex display={{ base: `flex`, sm: 'none' }}>
+            <HamburgerButton onClick={onOpen} ref={btnRef} />
+          </Flex>
+        </NavigationContainer>
+      </HeaderContainer>
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Munch Munch</DrawerHeader>
+
+          <StyledDrawerBody
+            display={{ base: 'flex' }}
+            flexDirection="column"
+            alignItems="stretch"
+            alignContent="stretch"
+          >
+            {MenuItems}
+          </StyledDrawerBody>
+
+          <DrawerFooter></DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
+const NavigationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 16px;
+`;
+const HeaderContainer = styled.header<{ borderColor?: string }>`
+  margin-bottom: 1.45rem;
+  border-bottom: 1px solid ${props => props.borderColor};
+`;
+const ButtonsContainer = styled.div`
+  display: flex;
+  &.loginButton {
+    margin-right: 8px;
+  }
+`;
+const StyledDrawerBody = styled(DrawerBody)`
+  button {
+    margin: 8px 0;
+  }
+`;
