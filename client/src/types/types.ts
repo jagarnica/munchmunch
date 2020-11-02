bb30b79f98d60fa081ce8fd6e02551e8a0993559
Jesus <gtechy100@gmail.com>
2020-11-01 16:40:07 -0800

    Added aws appsync and backend folder

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -62,3 +62,24 @@ export type AWSignInResponse = {
   };
   username: string;
 };
+
+/* eslint-disable  camelcase */
+export type AWSConfig = Readonly<{
+  aws_project_region: string;
+  aws_cognito_identity_pool_id: string;
+  aws_cognito_region: string;
+  aws_user_pools_id: string;
+  aws_user_pools_web_client_id: string;
+  oauth: {
+    domain: string;
+    scope: [string];
+    redirectSignIn: string;
+    redirectSignOut: string;
+    responseType: string;
+  };
+  federationTarget: string;
+  aws_appsync_graphqlEndpoint: string;
+  aws_appsync_region: string;
+  aws_appsync_authenticationType: string;
+}>;
+/* eslint-enable  camelcase */
