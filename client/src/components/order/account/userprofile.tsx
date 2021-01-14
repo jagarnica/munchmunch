ba9a5229a52e7b5d7120bf85920fe417634ccd91
Derek-M-Davis <52020929+Derek-M-Davis@users.noreply.github.com>
2021-01-13 23:22:14 -0800

    rough outline for user profile

--- client/src/components/order/account/userprofile.tsx
+++ client/src/components/order/account/userprofile.tsx
@@ -1,9 +1,53 @@
 import * as React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Center, Divider, HStack, Text} from '@chakra-ui/react';
 
 export const UserProfile = (): JSX.Element => {
   /**
    * Put the user profile stuff here!
    */
-  return <Box>Users profile!</Box>;
+  return (
+    <>
+      <Box bg="#ED8936" border="1px" borderRadius="3px">
+        <HStack p="5px">
+          <Box fontSize="3xl" w="150px" h="40px">
+            Profile
+          </Box>
+          <Button>Change Password</Button>
+        </HStack>
+        <Divider orientation="horizontal" />
+        <Box>
+          <HStack>
+            <Center w="100px" fontSize="3xl">
+              Name
+            </Center>
+            <Text fontSize="3xl">Last Name</Text>
+          </HStack>
+          <HStack>
+            <Text border="1px" p="10px">
+              Sample Text
+            </Text>
+            <Text border="1px" p="10px">
+              Sample Text
+            </Text>
+          </HStack>
+          <HStack>
+            <Center w="100px" fontSize="3xl">
+              Email
+            </Center>
+            <Center w="300px" fontSize="3xl">
+              Phone Number
+            </Center>
+          </HStack>
+          <HStack>
+            <Text border="1px" p="10px">
+              Sample Text
+            </Text>
+            <Text border="1px" p="10px">
+              Sample Text
+            </Text>
+          </HStack>
+        </Box>
+      </Box>
+    </>
+  );
 };
